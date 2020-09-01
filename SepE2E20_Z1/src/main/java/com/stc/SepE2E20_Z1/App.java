361970ac2a176aaa924a5d03bb133d52bcc64b1c
spselenim <pawal.satish@gmail.com>
2020-09-01 10:50:09 +0530

    First

---
+++ SepE2E20_Z1/src/main/java/com/stc/SepE2E20_Z1/App.java
@@ -0,0 +1,13 @@
+package com.stc.SepE2E20_Z1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
